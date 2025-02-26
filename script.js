fe62e66a691ae690d7daa06bdbc79b782c5e4844
amirullah 19 <amirullah58632@gmail.com>
2025-02-26 19:55:31 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.querySelector(".menu-toggle").addEventListener("click", function() {
-    document.querySelector(".nav-links").classList.toggle("active");
+// Ambil elemen
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("hamburger-menu");
+const menuIcon = menuToggle.querySelector("i");
+
+// Toggle menu saat tombol diklik
+menuToggle.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu.classList.toggle("active");
+
+    // Ubah ikon menu saat aktif/tidak
+    if (menu.classList.contains("active")) {
+        menuIcon.setAttribute("data-feather", "x");
+    } else {
+        menuIcon.setAttribute("data-feather", "menu");
+    }
+
+    // Perbarui ikon dengan feather.replace()
+    feather.replace();
 });
+
+
